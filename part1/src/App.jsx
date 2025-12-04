d506e8c63926223560b96648224bfb54022f1107
kenancosic <cosic.kenan@outlook.com>
2025-12-04 14:18:01 +0100

    Exercise 1.3
    Finished exercise 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,13 +3,21 @@ import Content from './Content';
 import Total from './Total';
 
 const App = () => {
+  
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
@@ -24,9 +32,9 @@ const App = () => {
         <p>
         {part3} {exercises3}
         </p> */}
-        <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]}></Content>
+        <Content parts={[part1, part2, part3]}></Content>
       {/* <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
-      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
+      <Total exercise1={part1.exercises} exercise2={part2.exercises} exercise3={part3.exercises} />
     </div>
   )
 }
--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -1,14 +1,14 @@
-const Part = (props) => {
+const Part = ({name, exercise}) => {
   return <div>
-    <p>{props.part} {props.exercise}</p>
+    <p>{name} {exercise}</p>
   </div>
 }
 
-const Content = ({ parts, exercises }) => {
+const Content = ({ parts }) => {
   return <div>
-    <Part part={parts[0]} exercise={exercises[0]} />
-    <Part part={parts[1]} exercise={exercises[1]} />
-    <Part part={parts[2]} exercise={exercises[2]} />
+    <Part name={parts[0].name} exercise={parts[0].exercises} />
+    <Part name={parts[1].name} exercise={parts[1].exercises} />
+    <Part name={parts[2].name} exercise={parts[2].exercises} />
   </div>
 }
 export default Content;
